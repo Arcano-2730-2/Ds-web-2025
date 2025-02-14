285e27e4991899ccad10204724e960d1d0d9e38a
Arcano-2730-2 <arcano.anjo@outlook.com>
2025-02-14 15:18:13 -0300

    Terceira lista

---
+++ Javascript/Atividades JS/Segunda lista/Terceira lista/script.js
@@ -0,0 +1,18 @@
+let contador = 0;
+function incrementarContador() {
+
+    contador++;
+    document.getElementById("contador").innerHTML = contador;
+}
+
+function capt() {
+    document.getElementById("texto2").innerText = document.getElementById("texto").value;
+}
+
+
+function bv(){
+    document.getElementById("texto1").innerHTML = "Seja bem-vindo!";
+}
+
+
+
